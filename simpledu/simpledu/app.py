e958cf35c03bd756fd24c338c8c5ac678738a851
leo <yeleihot@sina.com>
2019-10-04 18:15:39 +0800

    flask-sockets 聊天窗口

--- simpledu/simpledu/app.py
+++ simpledu/simpledu/app.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 from simpledu.config import configs
 from simpledu.models import db, User
 from flask_login import LoginManager
+from flask_sockets import Sockets
 
 
 def register_extensions(app):
@@ -20,11 +21,13 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    from .handlers import front, course, admin, live
+    from .handlers import front, course, admin, live, ws
     app.register_blueprint(front)
     app.register_blueprint(course)
     app.register_blueprint(admin)
     app.register_blueprint(live)
+    sockets = Sockets(app)
+    sockets.register_blueprint(ws)
 
 
 def create_app(config):
